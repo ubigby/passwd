c18b3c942429fd085edbbe99aca767d246c2af3d
ubigby <ubigby@protonmail.com>
2018-11-20 23:31:13 +0000

    update useradd

--- useradd.py
+++ useradd.py
@@ -1,12 +1,16 @@
 #create a new user
 
+import json
+
 print('Please enter a username:')
 user = str(input())
 print('Please enter a password:')
 passwd = int(input())
-print('Please enter a uid')
+print('Please enter a uid:')
 uid = int(input())
 
-f = open('passwd.txt', 'a')
-f.write('{}:{}:{}\n'.format(user, passwd, uid))
-f.close()
+
+account = {'name': user, 'password': passwd, 'UID': uid}
+
+with open('passwd.json', 'a') as file:
+    json.dump(account, file, indent=4)
